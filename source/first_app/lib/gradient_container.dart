2eb953fa895eac6e37fff9932b9f8db10fa56404
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2023-08-20 16:06:57 +0700

    Splitting Code Across Files

---
+++ source/first_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          const Color.fromARGB(255, 143, 92, 232),
+          Color.fromARGB(255, 88, 16, 244),
+          Color.fromARGB(2, 88, 16, 150),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: Center(
+        child: RichText(
+          text: TextSpan(
+              text: "hello world",
+              style: TextStyle(
+                  color: const Color.fromARGB(255, 235, 212, 136),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20)),
+        ),
+      ),
+    );
+  }
+}
--- source/first_app/lib/main.dart
+++ source/first_app/lib/main.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(body: GradientContainer()),
   ));
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          const Color.fromARGB(255, 143, 92, 232),
-          Color.fromARGB(255, 88, 16, 244),
-          Color.fromARGB(2, 88, 16, 150),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      ),
-      child: Center(
-        child: RichText(
-          text: TextSpan(
-              text: "hello world",
-              style: TextStyle(
-                  color: const Color.fromARGB(255, 235, 212, 136),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20)),
-        ),
-      ),
-    );
-  }
 }
